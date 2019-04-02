e3610e33bb4a34f6223f75d128db2adc03a7bb60
fleur-jv <fleur.jv@gmail.com>
2019-04-02 22:26:00 +0300

    add new tests & refactoring

--- features/main_page.feature
+++ features/main_page.feature
@@ -4,16 +4,45 @@ Feature: Main page vk.com
     When I am on the VK main page
     And The page title is Добро пожаловать | ВКонтакте
 
-  Scenario:  Registration
+  Scenario Outline:  Registration with valid data
+    And Fill the <first_name> and <last_name>
+    And Select the <birth_day>, <birth_month> and <birth_year>
+    Then Try to register
+    Examples:
+    |first_name|last_name|birth_day|birth_month|birth_year|
+    |Анна      |Каренина |1        |Января     |1970      |
+    |Anna      |Karenina |1        |Января     |1970      |
+    |Алексей   |Вронский |1        |Января     |1970      |
+    |Aleksey   |Vronskiy |1        |Января     |1970      |
+
+  Scenario Outline:  Registration with invalid name
+    And Fill the <first_name> and <last_name>
+    And Select the <birth_day>, <birth_month> and <birth_year>
+    Then Try to invalid name registration
+    Examples:
+    |first_name|last_name|birth_day|birth_month|birth_year|
+    |@@@       |@@@      |1        |Января     |1970      |
+    |&&&       |&&&      |1        |Января     |1970      |
+
+  Scenario: Registration with invalid date of birth
+    Then Try to register with empty fields
+    And Fill the Anna and Karenina
+    Then Try to invalid date registration
 
   Scenario: Authorization
+    And Fill login and password
+    Then Try to login
 
-  Scenario: Restore password
+  Scenario Outline: Links
+    Then Click at <link> and check <title>
+    Examples:
+    |link             |title                                         |
+    |Все продукты     |Продукты \| ВКонтакте                         |
+    |о компании       |О компании \| ВКонтакте                       |
+    |правила          |Пользовательское соглашение \| ВКонтакте      |
+    |реклама          |VK Business \| ВКонтакте                      |
+    |разработчикам    |Разработчикам \| ВКонтакте                    |
+    |Switch to English|Welcome! \| VK                                |
+    |index_forgot     |Восстановление доступа к странице \| ВКонтакте|
 
-  Scenario: Switch to English
-    Then Click at change language link
-
-  Scenario: Search
-
-  Scenario: Links
 
--- features/step_definitions/main_page_steps.rb
+++ features/step_definitions/main_page_steps.rb
@@ -2,13 +2,65 @@ When(/^I am on the VK main page$/) do
   visit "https://vk.com"
 end
 
-And(/^The page title is ([^"]*)$/) do |query|
-  expect(page).to have_content("#{query}")
+And(/^The page title is (.*)$/) do |title|
+  expect(page).to have_content("#{title}")
   @logger.info "Open VK successfully: Page title is \"#{page.title}\""
 end
 
-Then(/^Click at change language link$/) do
-  click_link 'Switch to English'
-  expect(page).to have_content('Welcome! | VK')
+And(/^Fill the (.*) and (.*)$/) do |first_name, last_name|
+  fill_in 'ij_first_name', with: "#{first_name}"
+  @first_name = first_name
+  fill_in 'ij_last_name', with: "#{last_name}"
+  @last_name = last_name
+end
+
+And(/^Select the (.*), (.*) and (.*)$/) do |birth_day, birth_month, birth_year|
+  find(:xpath, '//div[@class="ij_bday"]').click
+  find(:xpath, "//div[@class='ij_bday']//ul/li[@title='#{birth_day}']").click
+
+  find(:xpath, '//div[@class="ij_bmonth"]').click
+  find(:xpath, "//div[@class='ij_bmonth']//ul/li[@title='#{birth_month}']").click
+
+  find(:xpath, '//div[@class="ij_byear"]').click
+  find(:xpath, "//div[@class='ij_byear']//ul/li[@title='#{birth_year}']").click
+end
+
+Then(/^Try to register$/) do
+  click_button 'ij_submit'
+  expect(page).to have_content 'Регистрация | ВКонтакте'
+  @logger.info "Open registration page successfully: Page title is \"#{page.title}\""
+  @logger.info "You can confirm registration for #{@first_name} #{@last_name}"
+end
+
+Then(/^Try to invalid name registration$/) do
+  click_button 'ij_submit'
+  expect(page).to have_xpath '//b[contains(text(), "Пожалуйста, укажите Ваше имя и")]'
+  @logger.info "Error is displayed!"
+end
+
+Then(/^Try to invalid date registration$/) do
+  click_button 'ij_submit'
+  expect(page).to have_xpath '//b[contains(text(), "Пожалуйста, укажите дату рождения.")]'
+  @logger.info "Error is displayed!"
+end
+
+Then(/^Try to register with empty fields$/) do
+  step('Try to invalid name registration')
+end
+
+And(/^Fill login and password$/) do
+  fill_in 'index_email', with: 'email@gmail.com'
+  fill_in 'index_pass', with: 'password'
+end
+
+Then(/^Try to login$/) do
+  click_button 'index_login_button'
+  expect(page).to have_content 'Новости'
+  @logger.info "Successfull login: Page title is \"#{page.title}\""
+end
+
+Then(/^Click at (.*) and check (.*)$/) do |link, title|
+  click_link "#{link}"
+  expect(page).to have_content "#{title}"
   @logger.info "Switch language successfully: Page title is \"#{page.title}\""
 end
